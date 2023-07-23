9b58d4f0dd19b53c609b594fc811192d9f19e120
smitex1 <smithpraise61@gmail.com>
2023-07-23 13:42:31 +0100

    my first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+const App = () => {
+return (
+  <div>
+    <ProductList />
+    <Register />
+    <Login />
+    <Cart />
+  </div>
+);
+};
+
+export default App;
+
+
+
+
